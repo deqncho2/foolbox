04599d46c9fb8ee153ff7feeb04ca2781fc9d693
deqncho2 <deqn1996@gmail.com>
2019-01-23 22:51:51 +0000

    Update base.py

--- attacks/base.py
+++ attacks/base.py
@@ -125,23 +125,15 @@ def call_decorator(call_fn):
 
         assert a is not None
 
-        if a.distance.value == 0.:
-            warnings.warn('Not running the attack because the original input'
-                          ' is already misclassified and the adversarial thus'
-                          ' has a distance of 0.')
-        elif a.reached_threshold():
-            pass
-            warnings.warn('Not running the attack because the given treshold'
-                          ' is already reached')
-        else:
-            try:
-                _ = call_fn(self, a, label=None, unpack=None, **kwargs)
-                assert _ is None, 'decorated __call__ method must return None'
-            except StopAttack:
-                # if a threshold is specified, StopAttack will be thrown
-                # when the treshold is reached; thus we can do early
-                # stopping of the attack
-                logging.info('threshold reached, stopping attack')
+
+        try:
+            _ = call_fn(self, a, label=None, unpack=None, **kwargs)
+            assert _ is None, 'decorated __call__ method must return None'
+        except StopAttack:
+            # if a threshold is specified, StopAttack will be thrown
+            # when the treshold is reached; thus we can do early
+            # stopping of the attack
+            logging.info('threshold reached, stopping attack')
 
         if a.image is None:
             warnings.warn('{} did not find an adversarial, maybe the model'
